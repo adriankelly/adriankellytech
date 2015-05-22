5c18349167ac572bc4a0e9bd24dc7c2691032120
Adrian Kelly <hello@adriankelly.net>
2015-05-22 14:42:22 +0100

    basic drag function complete

--- js/script.js
+++ js/script.js
@@ -14,16 +14,39 @@ $('document').ready(function() {
 		}
 	});
 
-    // loop through each object (i.e. results.data[i])
     function postLoad(dataset) {
 		for (var i = 0; i < dataset.length - 1; i++) { //length is - 1because  header not included in appending posts e.g. name, email, date, text
 			$('.csv-target').append(
 				'<div class="post">'
-				+ '<span class="post-author">' + dataset[i].Name + '</span>'
+				+ '<h2 class="post-author">' + dataset[i].Name + '</h2>'
+				+ '<span class="post-date text-right">' + dataset[i].Date + '</span>'
 				+ '<address class="post-email">' + dataset[i].Email + '</address>' // add a link to email address?
-				+ '<span class="post-date">' + dataset[i].Date + '</span>'
+
 				+ '<p class="post-text">' + dataset[i].Text + '</p>'
 				+ '</div>')
 		};
 	}
+
+
+$(function() {
+    $("#sort").sortable({
+
+
+
+        tolerance: 'pointer',
+        scroll: true,
+        scrollSensitivity: 30,
+
+    	placeholder: 'sort-target',
+    	start: function(event, ui) {
+    		ui.item.addClass('current-drag');
+    	},
+    	stop: function(event, ui) {
+    		ui.item.removeClass('current-drag');
+    	}
+    });
+    $( "#sort" ).disableSelection();
+});
+
+
 });
