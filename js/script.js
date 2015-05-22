a8cb72ce04c976cf5d2700d1e8dd4d2cfbb6cfb3
Adrian Kelly <hello@adriankelly.net>
2015-05-22 12:49:03 +0100

    basic csv post loading complete

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
-var csvSource = '';
+$('document').ready(function() {
 
-Papa.parse(csvSource, {
-	header: true,
-	complete: function(results) {
-		console.log("Finished:", results.data);
+    var file = "\\js\\post-data.csv";
+
+	Papa.parse(file, {
+		header: true, // first line is interpreted as a header, not data
+		dynamicTyping: false, // respect types
+		delimiter: "|",
+		fastmode: true, //assumes no quoted fields exist
+		download: true,
+
+		complete: function(results){
+			postLoad(results.data);
+		}
+	});
+
+    // loop through each object (i.e. results.data[i])
+    function postLoad(dataset) {
+		for (var i = 0; i < dataset.length - 1; i++) { //length is - 1because  header not included in appending posts e.g. name, email, date, text
+			$('.csv-target').append(
+				'<div class="post">'
+				+ '<span class="post-author">' + dataset[i].Name + '</span>'
+				+ '<address class="post-email">' + dataset[i].Email + '</address>' // add a link to email address?
+				+ '<span class="post-date">' + dataset[i].Date + '</span>'
+				+ '<p class="post-text">' + dataset[i].Text + '</p>'
+				+ '</div>')
+		};
 	}
 });
