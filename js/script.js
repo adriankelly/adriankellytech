457e893061b32bfb37f1b22d4daf0b7d589fd3ef
Adrian Kelly <hello@adriankelly.net>
2015-05-22 17:05:50 +0100

    completed styling and functionality of post shifting and selecting

--- js/script.js
+++ js/script.js
@@ -1,52 +1,73 @@
 $('document').ready(function() {
 
-    var file = "\\js\\post-data.csv";
+	if (Modernizr.mq('only all and (max-width: 992px)')) {
 
-	Papa.parse(file, {
-		header: true, // first line is interpreted as a header, not data
-		dynamicTyping: false, // respect types
-		delimiter: "|",
-		fastmode: true, //assumes no quoted fields exist
-		download: true,
+		var file = "\\js\\post-data.csv";
 
-		complete: function(results){
-			postLoad(results.data);
+		/**
+		 * Parse csv into JS readable array of objects
+		 */
+
+		Papa.parse(file, {
+			header: true, // first line of csv is a header
+			delimiter: "|",
+			fastmode: true, // no quoted fields exist in csv
+			download: true,
+			complete: function(results){
+				postLoad(results.data);
+			}
+		});
+
+		/**
+		 * Iterate over dataset and append object properties
+		 */
+
+		function postLoad(dataset) {
+			for (var i = 0; i < dataset.length - 1; i++) {
+				$('.csv-target').append(
+					'<div class="post">'
+					+ '<h3 class="post-author">' + dataset[i].Name + '</h3>'
+					+ '<span class="post-date text-right">' + dataset[i].Date + '</span>'
+					+ '<address class="post-email"><a href="mailto:#">' +dataset[i].Email + '</a></address>'
+					+ '<p>' + dataset[i].Text + '</p>'
+					+ '</div>'
+				);
+			};
+
+			// Enable individual post to be higlighted on click
+			$('.post').on('click', function(){
+				if ($(this).hasClass('selected')) {
+					$(this).removeClass('selected');
+				} else {
+					$(this).addClass('selected').siblings().removeClass('selected');
+				}
+			});
 		}
-	});
-
-    function postLoad(dataset) {
-		for (var i = 0; i < dataset.length - 1; i++) { //length is - 1because  header not included in appending posts e.g. name, email, date, text
-			$('.csv-target').append(
-				'<div class="post">'
-				+ '<h2 class="post-author">' + dataset[i].Name + '</h2>'
-				+ '<span class="post-date text-right">' + dataset[i].Date + '</span>'
-				+ '<address class="post-email">' + dataset[i].Email + '</address>' // add a link to email address?
-
-				+ '<p class="post-text">' + dataset[i].Text + '</p>'
-				+ '</div>')
-		};
-	}
-
-
-$(function() {
-    $("#sort").sortable({
 
 
 
-        tolerance: 'pointer',
-        scroll: true,
-        scrollSensitivity: 30,
 
-    	placeholder: 'sort-target',
-    	start: function(event, ui) {
-    		ui.item.addClass('current-drag');
-    	},
-    	stop: function(event, ui) {
-    		ui.item.removeClass('current-drag');
-    	}
-    });
-    $( "#sort" ).disableSelection();
-});
+		/**
+		 * jQuery UI sortable used to enable drag and drop of div elements
+		 */
 
+		$("#sort").sortable({
+			tolerance: 'pointer',
+			scroll: true,
+			scrollSensitivity: 30,
+			opacity: 0.5,
+			placeholder: 'sort-target',
+			start: function(event, ui) {
+				ui.item.addClass('current-drag');
+			},
+			stop: function(event, ui) {
+				ui.item.removeClass('current-drag selected');
+			}
+		});
 
+		$( "#sort" ).disableSelection();
+
+	} else {
+		window.location = "http://www.candyspace.com"; //redirect for desktop users
+	};
 });
