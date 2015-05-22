49f5af2bc4c04c6e808aa1d884fa074c24d49616
Adrian Kelly <hello@adriankelly.net>
2015-05-22 18:46:53 +0100

    correcting scroll on mobile

--- js/script.js
+++ js/script.js
@@ -49,6 +49,20 @@ if ($(touch.target).is(".post")) {
   event.preventDefault();
 }
 
+$.fn.extend({
+    disableSelection: function() {
+        this.each(function() {
+            this.onselectstart = function() {
+                return false;
+            };
+            this.unselectable = "on";
+            $(this).css('-moz-user-select', 'none');
+            $(this).css('-webkit-user-select', 'none');
+        });
+        return this;
+    }
+});
+
 
 
 		/**
