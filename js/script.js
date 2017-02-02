1d0bb82cb12513d9ba2f86933471d50653572058
Adrian Kelly <hello@adriankelly.net>
2017-02-02 12:28:52 +0100

    Colour of BG and posts changed, modified readme

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-$('document').ready(function() {
+$('document').ready(function () {
 
 	if (Modernizr.mq('only all and (max-width: 992px)')) {
 
-		var file = "http://adriankelly.net/adriankellytech/js/post-data.csv";
+		var file = "/js/post-data.csv";
 
 		/**
 		 * Parse csv into JS readable array of objects
@@ -13,7 +13,7 @@ $('document').ready(function() {
 			delimiter: "|",
 			fastmode: true, // no quoted fields exist in csv
 			download: true,
-			complete: function(results){
+			complete: function (results) {
 				postLoad(results.data);
 			}
 		});
@@ -24,16 +24,16 @@ $('document').ready(function() {
 
 		function postLoad(dataset) {
 			for (var i = 0; i < dataset.length - 1; i++) {
-				$('.csv-target').append('<div class="post">' + '<h3 class="post-author">' + dataset[i].Name + '</h3>' + '<span class="post-date text-right">' + dataset[i].Date + '</span>'	+ '<address class="post-email"><a href="mailto:#">' +dataset[i].Email + '</a></address>' + '<p>' + dataset[i].Text + '</p>'	+ '</div>'
+				$('.csv-target').append('<div class="post">' + '<h3 class="post-author">' + dataset[i].Name + '</h3>' + '<span class="post-date text-right">' + dataset[i].Date + '</span>' + '<address class="post-email"><a href="mailto:#">' + dataset[i].Email + '</a></address>' + '<p>' + dataset[i].Text + '</p>' + '</div>'
 				);
 			}
 
 			// Enable individual post to be higlighted on click
-			$('.post').click(function(){
+			$('.post').click(function () {
 				$(this).toggleClass('selected').siblings().removeClass('selected');
-		        $(this).css('-moz-user-select', 'none');
-        		$(this).css('-webkit-user-select', 'none');
-				});
+				$(this).css('-moz-user-select', 'none');
+				$(this).css('-webkit-user-select', 'none');
+			});
 		}
 
 		/**
@@ -47,17 +47,17 @@ $('document').ready(function() {
 			scrollSensitivity: 30,
 			opacity: 0.5,
 			placeholder: 'sort-target',
-			start: function(event, ui) {
+			start: function (event, ui) {
 				ui.item.addClass('current-drag');
 			},
-			stop: function(event, ui) {
+			stop: function (event, ui) {
 				ui.item.removeClass('current-drag selected');
 			}
 		});
 
-		$( "#sort" ).disableSelection();
+		$("#sort").disableSelection();
 
 	} else {
-		window.location = "http://www.candyspace.com"; //redirect for desktop users
+		window.location = "http://adriankelly.net"; //redirect for desktop users
 	}
 });
